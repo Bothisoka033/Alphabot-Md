bd505247d35159691ae12eef0b0d073e12ce9fb4
Bothisoka033 <106675720+Bothisoka033@users.noreply.github.com>
2022-06-01 23:24:55 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,27 +12,27 @@ global.APIKeys = {
 }
 
 // setting 
-global.autoread = false // auto read pesan / message
-//global.autorecording = true //status auto merekam ( auto record )
-//global.autoketik = false //status auto mengetik (auto typing)
+global.autoread = true // auto read pesan / message
+//global.autorecording = false //status auto merekam ( auto record )
+//global.autoketik = true //status auto mengetik (auto typing)
 //global.available = false //status online (online)
 
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
+global.botname = "𝚈𝚞𝚔𝚒𝚗𝚒𝚔𝚘 - 𝙱𝚘𝚝" //namabot kalian
+global.ownername= "𝙵𝚒𝚔𝚛𝚒𝚒𝚂𝚔𝚢" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.github = "https://𝙵𝚒𝚔𝚛𝚒𝚒𝚂𝚔𝚢.github.io/" //bebas
+global.email = "𝙵𝚒𝚔𝚛𝚒𝚒𝚂𝚔𝚢@gmail.com" //bebas
 global.region = "Indonesia" //bebas
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.ownernomer = "6281233738677" // nomor wa kalian
+global.ownernomerr = "+6281233738677" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["62887435047326","62887435047326","918156874290"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.owner = ["6281233738677","6281233738677","6281233738677"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© 𝚈𝚞𝚔𝚒𝚗𝚒𝚔𝚘 - 𝙱𝚘𝚝' //sticker wm ubah
+global.author = 'Di Buat Oleh 𝚈𝚞𝚔𝚒𝚗𝚒𝚔𝚘 - 𝙱𝚘𝚝' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
